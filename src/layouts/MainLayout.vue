e7e4854aee3e73bee202048ce447e806ade47e13
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-01-16 22:52:22 +0200

    implement basic home page styling

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,25 +1,60 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
+    <q-header 
+      class="bg-white text-grey-10"
+      bordered
+    >
+      <q-toolbar class="constrain">
+        <q-btn 
+          flat 
+          round 
+          class="large-screen-only q-mr-sm"
+          icon="eva-camera-outline"
+          to="/camera"
+          size="18px"
           dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+        <q-separator 
+          class="large-screen-only"
+          vertical 
+          spaced
+        />
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasagram
+        </q-toolbar-title>
+        <q-btn 
+          flat 
+          round 
+          class="large-screen-only"
+          icon="eva-home-outline"
+          to="/"
+          size="18px"
+          dense
         />
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
-    
+    <q-footer 
+      class="bg-white small-screen-only"
+      bordered
+    >
+        <!-- <q-toolbar-title>Footer</q-toolbar-title> -->
+      <q-tabs 
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab 
+          to="/"
+          icon="eva-home-outline" 
+        />
+        <q-route-tab 
+          to="/camera"
+          icon="eva-camera-outline" 
+        />
+      </q-tabs>
+    </q-footer>
 
     <q-page-container>
       <router-view />
@@ -39,3 +74,18 @@ export default {
   }
 }
 </script>
+
+<style lang="sass">
+  .q-toolbar
+    @media (min-width: $breakpoint-sm-min)
+      height: 77px
+  .q-toolbar__title
+    font-size: 30px
+    @media (max-width: $breakpoint-xs-max)
+      text-align: center
+  .q-footer
+    .q-tab__icon
+      font-size: 30px
+  
+
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,10 +1,5 @@
 <template>
   <q-page class="flex flex-center">
-    <!-- <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    > -->
     <h5>Home Page</h5>
   </q-page>
 </template>
