1f8b7b3204fd166dd914d3959b89278b0517a95a
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-01-17 08:32:25 +0200

    implement basic PageCamera layout

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,6 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img
+        class="full-width"
+        src="https://picsum.photos/id/1025/600/400"
+        alt="">
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        round 
+        color="grey-10"
+        icon="eva-camera" 
+        size="lg"
+      />
+    </div>
   </q-page>
 </template>
 
@@ -9,3 +22,8 @@ export default {
   name: 'PageCamera'
 }
 </script>
+<style lang="sass">
+  .camera-frame 
+    border: 2px solid $grey-10
+    border-radius: 10px
+</style>
