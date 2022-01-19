c5208fb5d5efb554ed3020cac77ff7dd90e4481e
Salvador Anselmo Rombe <rostovsaor@gmail.com>
2022-01-19 18:46:08 +0200

    configure firebase in backend

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@
 */
 
   const express = require('express')
+  const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+  const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+  
 
+  
 /*
   config - express
 */
@@ -11,14 +15,34 @@
   const app = express()
 
 /*
+  config firebase
+*/
+
+  const serviceAccount = require('./serviceAccountKey.json');
+    
+  initializeApp({
+    credential: cert(serviceAccount)
+  });
+
+  const db = getFirestore();
+
+/*
   endpoint
 */
 
-  app.get('/', (request, response) => {
-    response.send('I love Node!!')
+  app.get('/posts', (request, response) => {
+    let posts = []
+    db.collection('posts').get().then(snapshot => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+        posts.push(doc.data())
+      });
+      response.send(posts)
+    })
   })
 
 /*
-  listend
+  listen
 */
-  app.listen(3000)
+  // app.listen(3000)
+  app.listen(process.env.PORT || 3000)
